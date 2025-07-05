2859a4cf8f839ae88da9bddaa05019733bff99cb
Arruda98 <jeffersonarruda98@gmail.com>
2025-07-05 16:03:18 -0300

    Add files via upload

---
+++ Desafio 3/Desafio 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Desafio3
+{
+    class Tabuada
+    {
+        public static void Main(string[] args)
+        {
+            double numero = 0;
+            Console.WriteLine("Tabuada do Koj1One");
+
+            Console.WriteLine("Digite o valor que você deseja ver a Tabuada: ");
+            numero = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+            }
+        }
+
+    }
+}
+
